3c6f8c318390686de5c74f10e37cdcc59968aa3a
wchen928 <lchen.msc2019@ivey.ca>
2020-07-09 09:48:38 -0700

    feat: complete all functions

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,41 @@
 import sys
-
+import pandas as pd 
+import numpy as np
+import sqlite3
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, left_on='id', right_on='id', how='outer')
+    return df
 
 def clean_data(df):
-    pass
+    #split categories into separate category columns
+    categories = df['categories'].str.split(";", expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
 
+    #convert category values to nums 0 & 1
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        categories[column] = pd.to_numeric(categories[column])
+
+    #reaplace the category column with the new columns
+    df.drop(['categories'], inplace=True, axis=1)
+    df = pd.concat([df, categories], axis=1)
+
+    #remove duplicates
+    if df.duplicated().sum() > 0:
+        df.drop_duplicates(inplace=True)
+    
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///' + database_filename, pool_pre_ping=True)
+    #table_name = database_filename.replace(".db","").lower()
+    df.to_sql('DisasterResponse', engine, index=False, if_exists='replace')
 
 
 def main():
